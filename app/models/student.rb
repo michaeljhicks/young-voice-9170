1988d6ae67988fa8fdfa19c0ea41368982ecbde9
Michael Hicks <hicks.michael04@gmail.com>
2022-02-15 10:44:35 -0700

    Add model relationships to professor_student, professor, and student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student <ApplicationRecord
-
+  has_many: professor_students
+  has_many: professors, through: :professor_students
+  validates_presence_of :name, presence: true
+  validates_presence_of :age, presence: true
+  validates_presence_of :house, presence: true
 end
